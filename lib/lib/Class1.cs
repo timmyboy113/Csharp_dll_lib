174b668702432e16a973dc8feeb92235d84bd86c
timmyboy113 <flarp113@googlemail.com>
2016-03-22 20:48:47 +0100

    init
    init commit

---
+++ lib/lib/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lib
+{
+    public class Test
+    {
+        public static string hello_world()
+        {
+            return ("Hello World");
+        }
+
+        public static string echo(string to_echo)
+        {
+            return (to_echo);
+        }
+    }
+}
